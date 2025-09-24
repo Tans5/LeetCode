344c489e199a51c71934347ff3d0941b72e6757e
tans.tan <tans.tan096@gmail.com>
2025-09-24 22:46:38 +0800

    Add interview.

---
+++ src/com/leetcode/practice/interview/Tencent1.java
@@ -0,0 +1,22 @@
+package com.leetcode.practice.interview;
+
+
+/**
+ * 给定入栈顺序为 1, 2, 3, ..., n（即固定顺序），求有多少种可能的出栈顺序。
+ *
+ * n = 1, [1]
+ * n = 2, [1, 2], [2, 1]
+ * n = 3, [1, 2, 3], [2, 1, 3], [3, 2, 1], [3, 1, 2], [2, 3, 1]
+ */
+public class Tencent1 {
+
+    /**
+     *
+     * @param n 入栈元素数量
+     * @return
+     */
+    int popStackWays(int n) {
+        // TODO:
+        return n;
+    }
+}
